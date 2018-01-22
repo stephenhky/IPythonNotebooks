d6c1df35ad55d427bd875777044a5aa396b50b87
stephenhky <stephenhky@yahoo.com.hk>
2018-01-22 00:08:44 -0500

    using textMineR and snowballC in R codes

--- shorttext/DTM/TextMineRInauguralDTM.R
+++ shorttext/DTM/TextMineRInauguralDTM.R
@@ -1,5 +1,12 @@
 
 library(textmineR)
+library(SnowballC)
 
 usprez.df<- read.csv('inaugural.csv', stringsAsFactors = FALSE)
-
+dtm<- CreateDtm(usprez.df$speech, 
+                doc_names = usprez.df$yrprez, 
+                ngram_window = c(1, 1),
+                lower = TRUE,
+                remove_punctuation = TRUE,
+                remove_numbers = TRUE,
+                stem_lemma_function = wordStem)
