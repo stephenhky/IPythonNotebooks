7d9b8cd5e85ce5446d0a6ed57d4a920234d71017
stephenhky <stephenhky@yahoo.com.hk>
2018-01-29 14:28:04 -0500

    model trained for iterations=20

--- Seq2Seq/norvig_shorttext_chatbot_big_training.py
+++ Seq2Seq/norvig_shorttext_chatbot_big_training.py
@@ -18,7 +18,7 @@ latent_dim = numchars + 20
 
 # training
 seq2seqer = shorttext.generators.CharBasedSeq2SeqGenerator(chartovec_encoder, latent_dim, 120)
-seq2seqer.train(text, epochs=100)
+seq2seqer.train(text, epochs=20)
 
 # save the model
-seq2seqer.save_compact_model('norvigtxt_iter5model_big.bin')
+seq2seqer.save_compact_model('norvigtxt_iter5model_iter20.bin')
